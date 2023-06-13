dd209d5d9969629a5899772a1c073ea37f4b61ff
bm_mukesh <mukesh.thankar@bemasterly.com>
2023-06-13 14:48:55 +0530

    Initial commit

---
+++ apps/bm-2.0/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="bm-2.0" />
+    </StyledApp>
+  );
+}
+
+export default App;
